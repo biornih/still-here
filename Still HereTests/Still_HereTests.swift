6ecf2dd6f3ef1bcfe440c3ad759b48cbe201fc1e
Biorni Hasmegaj <biorni.hasmegaj@st.oth-regensburg.de>
2026-08-02 21:37:28 +0200

    Initial Commit

---
+++ Still HereTests/Still_HereTests.swift
@@ -0,0 +1,19 @@
+//
+//  Still_HereTests.swift
+//  Still HereTests
+//
+//  Created by Biorni on 02.08.26.
+//
+
+import Testing
+@testable import Still_Here
+
+struct Still_HereTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
